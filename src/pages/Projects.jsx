05210a16e771f8ac049313fe3f46f2c67e960621
bakuyy <sophieyang12345@gmail.com>
2024-10-15 01:59:34 -0400

    Added typewriter

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from 'react';
+import Typewriter from 'typewriter-effect';
 
 const Projects = () => {
   return (
-    <div>
-      
+    <div className='flex justify-center items-center'>
+    <Typewriter
+    className='typewriter'
+      onInit={(typewriter) => {
+        typewriter.typeString('IDEAS')
+        .pauseFor(1500)
+        typewriter.typeString('·················')
+        .pauseFor(1500)
+        typewriter.typeString('REALITY')
+        .start();
+      }}
+    />
     </div>
-  )
+  );
 }
 
-export default Projects
+export default Projects;
