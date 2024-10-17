06a9099b4c9e3f86de2dd4b58aad5b5f714b5fc1
bakuyy <sophieyang12345@gmail.com>
2024-10-16 21:48:38 -0400

    added animation to resume page

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import '../styling/Resume.css';
 import { IoCloudDownloadOutline } from "react-icons/io5";
-
+import Resumeman from "../assets/Resumeman.gif";
 
 const Resume = () => {
   return (
     <div className="resume-container">
+      <div className="resumeman-container">
+        <img className="resumeman" src={Resumeman} alt="Resumeman" />
+      </div>
       <iframe 
         className="resume-pdf" 
         src="https://drive.google.com/file/d/1CieNr5sNA2OJ_IQey6R163l-lBz77JJC/preview" 
@@ -19,7 +22,6 @@ const Resume = () => {
         rel="noopener noreferrer"
       >
         <IoCloudDownloadOutline/>
-        
       </a>
     </div>
   );
